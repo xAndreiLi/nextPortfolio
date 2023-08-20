daead97a422d31d246a61c8f17a75735d8257fdb
xLunith <liandrei2000@gmail.com>
2023-08-20 04:09:11 -0700

    againnnnn

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux'
 
 import Home from '../components/home/home'
 import store from '../util/store'
-import { Header } from '../components/header'
 
 export const Root: NextPage = () => {
   return (
